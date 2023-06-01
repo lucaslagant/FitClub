0ebd1bee7093c4dc8778fc7d11e78d663b47c4d6
lucas <lucas@home.home>
2023-06-01 19:23:48 +0200

    join

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import './Testimonials.css'
 import { testimonialsData } from '../../data/testimonialsData'
+import leftArrow from '../../assets/leftArrow.png'
+import rightArrow from '../../assets/rightArrow.png'
 
 const Testimonials = () => {
     const [selected, setSelected] = useState(0);
@@ -21,7 +23,27 @@ const Testimonials = () => {
                     - {testimonialsData[selected].status}
                 </span>
             </div>
-            <div className="right-t"></div>
+            <div className="right-t">
+                <div></div>
+                <div></div>
+                <img src={testimonialsData[selected].image}/>
+                <div className="arrows">
+                    <img 
+                    onClick={() =>{
+                        selected===0
+                        ? setSelected(tLenght -1)
+                        : setSelected((prev) => prev -1)
+                    }}
+                    src={leftArrow} />
+                    <img
+                    onClick={() =>{
+                        selected === tLenght - 1
+                        ? setSelected(0)
+                        : setSelected((prev) => prev + 1);
+                    }}
+                    src={rightArrow} />
+                </div>
+            </div>
         </div>
     )
 }
